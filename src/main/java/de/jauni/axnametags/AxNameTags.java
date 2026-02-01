86b53577374f0b83031ef23c421ea7bdf274082a
JauniKappauni <social.jauni@gmail.com>
2026-02-01 17:20:43 +0100

    init: initialized project with basic structure

---
+++ src/main/java/de/jauni/axnametags/AxNameTags.java
@@ -0,0 +1,17 @@
+package de.jauni.axnametags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxNameTags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
